d4920cd098a43e769bf641e73a0945d2fa33a13e
Mahmoud Gamal <mahmoudgz2003@gmail.com>
2019-05-20 16:46:03 +0200

    delete node from array

--- deleteNode.js
+++ deleteNode.js
@@ -43,5 +43,10 @@ var findMinNode=function(node){
 }
 
 var root = new TreeNode(5)
+root.left=new TreeNode(3)
+root.left.right=new TreeNode(4)
+root.left.left=new TreeNode(2)
+root.right=new TreeNode(6)
+root.right.right=new TreeNode(7)
 root=deleteNode(root,3)
 console.log(root)

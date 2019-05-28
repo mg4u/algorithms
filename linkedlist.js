5cc46e5bce945d9c329d41cc5506e23a3dfd808a
Mahmoud Gamal <mahmoudgz2003@gmail.com>
2019-05-28 05:04:07 +0200

    linked list

--- linkedlist.js
+++ linkedlist.js
@@ -1,101 +1,96 @@
 class Node {
-  constructor() {
-    this.value = null;
-    this.next = null;
-  }
+    constructor() {
+        this.value = null;
+        this.next = null;
+    }
 }
 
 class LinkedList {
-  constructor() {
-    // root of a binary seach tree 
-    this.head = null;
-    this.linkedList = []
-  }
+    constructor() {
+        // root of a binary seach tree 
+        this.head = null;
+        this.linkedList = []
+    }
 
-  getHead() {
-    return this.head
-  }
+    getHead() {
+        return this.head
+    }
 
-  toArray() {
-    let output = [],
-      node = this.head
-    while (node.next) {
-      output.push(node.value)
-      node = node.next
+    toArray() {
+        let output = [],
+            node = this.head
+        while (node.next) {
+            output.push(node.value)
+            node = node.next
+        }
+        output.push(node.value)
+        console.log('output', output)
+        return output
     }
-    console.log('output', output)
-    return output
-  }
-  addToList(val) {
-    let output = [],
-      node = this.head
-    if (this.head == null) {
-      let newNode = new Node()
-      newNode.value = val
-      this.head = newNode
-      return;
+
+    addToList(val) {
+        let output = [],
+            node = this.head
+        if (this.head == null) {
+            let newNode = new Node()
+            newNode.value = val
+            newNode.next = null
+            this.head = newNode
+            // console.log('head node',this.head)
+            return;
+        }
+        if (this.head.next == null) {
+            let newNode = new Node()
+            let headNode=this.head
+            newNode.value = val
+            if(this.head.value>val){
+                this.head = newNode
+                this.head.next = headNode
+            }else{
+                this.head.next = newNode
+            }
+            // console.log('head node',this.head)
+            return;
+        }
+        let addAfterLoop = true;
+        while (node.next) {
+            console.log(node.value,node.next.value)
+            // output.push(node.value)
+            if (node.value < val && node.next.value >= val) {
+                addAfterLoop = false;
+                let newNode = new Node()
+                newNode.value = val
+                newNode.next = node.next
+                node.next = newNode
+                break;
+            } else {
+                node = node.next
+            }
+        }
+        // console.log('-------------- of val ',val,output)
+        if (addAfterLoop) {
+            console.log('addAfterLoop',val,addAfterLoop,node)
+            let newNode = new Node()
+            newNode.value = val
+            newNode.next = null
+            node.next = newNode
+        }
     }
-    let addAfterLoop = true;
-    while (node.next) {
-      //output.push(node.value)
-      if (node.value < val && node.next.value >= val) {
-        addAfterLoop = false;
-        let newNode = new Node()
-        newNode.value = val
-        node.next.next = node.next
-        node.next = newNode
-        break;
-      } else {
-        node = node.next
-      }
-      console.log(node.next)
-    }
-    console.log('--------------')
-    if (addAfterLoop) {
-      let newNode = new Node()
-      newNode.value = val
-      node.next = newNode
-    }
-  }
-  addToList2(val) {
-    console.log('add ', this.head, val)
-    //1- if this.head is null create a new node and set it as the this.head of the list
-    if (this.head === null) {
-      let newNode = new Node()
-      newNode.value = val
-      this.head = newNode
-      //this.head.next=newNode
-      return newNode
-    }
-    //2- if there is a this.head with no next , set the new node with the new value as next node
-    if (this.head.value < val && !this.head.next) {
-      let newNode = new Node()
-      newNode.value = val
-      this.head.next = newNode
-      return newNode
-    }
-    //3- 
-    if (this.head.value < val && this.head.next.value < val) {
-      // node=this.addToList(val)
-      return node
-    } else if (this.head.value < val && this.head.next.val >= val) {
-      let newNode = new Node()
-      newNode.value = val
-      newNode.next = this.head.next
-      this.head.next = newNode
-      return newNode
-    }
-  }
 }
 console.log('Practice makes Perfect!');
 let ls = new LinkedList()
 
 ls.addToList(1)
+ls.toArray()
 ls.addToList(2)
+ls.toArray()
 ls.addToList(4)
+ls.toArray()
 ls.addToList(7)
-
+ls.toArray()
 ls.addToList(5)
+ls.toArray()
+
 ls.addToList(6)
 
 ls.toArray()

bb8241fdddd72c267cc9a4d300dac4b66a6c1718
Mahmoud Gamal <mahmoudgz2003@gmail.com>
2019-06-22 12:13:00 +0200

    minmumParseTime.js

--- minmumParseTime.js
+++ minmumParseTime.js
@@ -1,2 +1,20 @@
 // Asked by amazone
 // get minmum time required to parse streaming files using 2 files per time
+// 
+function minimumTime(numOfSubFiles, files){
+    files=files.sort(function(a, b){return a-b});
+    var minimumTime=0,
+    lastVal=files[0],
+    sums=[]
+    for(var i=1;i<files.length;i++){
+      lastVal+=files[i]
+      sums.push(lastVal);//+files[i])
+    }
+    for (var i = sums.length - 1; i >= 0; i--) {
+      minimumTime+=sums[i]
+    };
+    console.log(minimumTime)
+    // WRITE YOUR CODE HERE
+  }
+minimumTime(4,[8,4,6,12])
+minimumTime(4,[1,2,5,10,35,89])

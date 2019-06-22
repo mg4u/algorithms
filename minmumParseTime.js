ba8e3321403947755128f3205a5732096d9dcae8
Mahmoud Gamal <mahmoudgz2003@gmail.com>
2019-06-22 12:18:29 +0200

    minmumParseTime.js

--- minmumParseTime.js
+++ minmumParseTime.js
@@ -1,6 +1,11 @@
 // Asked by amazone
 // get minmum time required to parse streaming files using 2 files per time
-// 
+// assume file size is 8,4,6,12 the the minmum time required is 58
+// where 6+4 produce file with size => [10]
+// then the new produced file 10 + 8 produce file with size => [18]
+// then the new produced file 18 + 12 produce file with size => [30]
+// so the total time required to parse all these files = sum of totoal time required to parse 2 files per time
+// 10+18+30=58
 function minimumTime(numOfSubFiles, files){
     files=files.sort(function(a, b){return a-b});
     var minimumTime=0,
@@ -15,7 +20,6 @@ function minimumTime(numOfSubFiles, files){
       minimumTime+=sums[i]
     };
     console.log(minimumTime)
-    // WRITE YOUR CODE HERE
-  }
+}
 minimumTime(4,[8,4,6,12])
 minimumTime(4,[1,2,5,10,35,89])

1f9e3755c961944c7abe4ef1e487060a99e856d2
Mahmoud Gamal <mahmoudgz2003@gmail.com>
2019-08-19 12:18:57 +0200

    binary Cisco prop

--- binary.js
+++ binary.js
@@ -1,6 +1,4 @@
 /*
-Good morning! Here's your coding interview problem for today.
-
 This problem was asked by Cisco.
 
 Given an unsigned 8-bit integer, swap its even and odd bits. The 1st and 2nd bit should be swapped, the 3rd and 4th bit should be swapped, and so on.

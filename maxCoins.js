7c11741c752f70dd725c4a0fa59d4ca90f240ac4
Mahmoud Gamal <mahmoudgz2003@gmail.com>
2019-09-01 12:53:16 +0200

    finish get max expensive route to top left from right bottom and from top left to right bottom

--- maxCoins.js
+++ maxCoins.js
@@ -1,4 +1,7 @@
 /*
+
+// Developed By: Mahmoud Gamal
+
 This question was asked by Zillow.
 
 You are given a 2-d matrix where each cell represents number of coins in that cell. Assuming we start at matrix[0][0], and can only move right or down, find the maximum number of coins you can collect by the bottom right corner.
@@ -12,44 +15,35 @@ The most we can collect is 0 + 2 + 1 + 5 + 3 + 1 = 12 coins.
 
 */
 
-// TODO: IN PROGRESS
+
 
 let matrix=[
 	[0, 3, 1, 1],
 	[2, 0, 0, 4],
 	[1, 5, 3, 1]
 ]
-let sum=0
 
 function getMaxSumToBottomRight (mat, x, y) {
-	var val=0
 	if (x >= mat.length || y >= mat[0].length){
 		return 0
 	} else{
-	    val= mat[x][y] +   
+	    return mat[x][y] +   
 	            Math.max( getMaxSumToBottomRight(mat, x + 1, y + 1), 
 	            getMaxSumToBottomRight(mat, x + 1, y),  
 	            getMaxSumToBottomRight(mat, x, y + 1) )
 	}
-
-	return val
 }
 
 function getMaxSumToTopLeft (mat, x, y) {
-	var val=0
 	if (x < 0 || y < 0){
 		return 0
 	} else{
-	    val= mat[x][y] +   
+	    return mat[x][y] +   
 	            Math.max( getMaxSumToTopLeft(mat, x - 1, y - 1), 
 	            getMaxSumToTopLeft(mat, x - 1, y),  
 	            getMaxSumToTopLeft(mat, x, y - 1) )
 	}
-
-	return val
 }
-
-
 
 console.log(getMaxSumToBottomRight(matrix,0,0)) // allowed directions are right and bottom
 let sX=matrix.length-1

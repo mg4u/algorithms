5c3ec69a12ce27c3c3c8d1e61645852ebc6bfac5
Mahmoud Gamal <mahmoudgz2003@gmail.com>
2019-09-01 13:53:53 +0200

    Add some info in the file

--- binary.js
+++ binary.js
@@ -1,4 +1,5 @@
 /*
+Developed By: Mahmoud Gamal
 This problem was asked by Cisco.
 
 Given an unsigned 8-bit integer, swap its even and odd bits. The 1st and 2nd bit should be swapped, the 3rd and 4th bit should be swapped, and so on.

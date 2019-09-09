825ea0224cdf131ca666db55572d4e429465f7f2
Mahmoud Gamal <mahmoudgz2003@gmail.com>
2019-09-09 12:45:52 +0200

    easy way to get square root

--- squareRoot.js
+++ squareRoot.js
@@ -3,6 +3,8 @@ Developed by Mahmoud Gamal
 
 Given a real number n, find the square root of n. For example, given n = 9, return 3.
 
+// Note that this way is perfect for numbers with maximum 17 chars length like 9999999999999999
+// more than that there is another way to calcualte the square root which is more complex than the following way
 */
 
 function squareRoot (n) {
@@ -21,7 +23,7 @@ function squareRoot (n) {
 	console.log('root', root);
 	return root
 }
-var num = 654
+var num = 9999999999999999
 const jsRoot = Math.round( Math.sqrt( num ) * 100 )/100
 const devRoot = squareRoot( num )
 console.log('JS root ',jsRoot);
